238acc37d3f514ed819f55002eba82041c657326
sunruonan <rnsun9302@163.com>
2020-03-01 21:32:21 +0800

    freemall commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <div class="md-modal modal-msg md-modal-transition" v-bind:class="{'md-show':mdShow}">
+        <div class="md-modal-inner">
+          <div class="md-top">
+            <button class="md-close" @click="closeModal">关闭</button>
+          </div>
+          <div class="md-content">
+            <div class="confirm-tips">
+              <!--定义插槽，要起名字，不起名字时默认只有一个插槽-->
+              <slot name="message"></slot>
+            </div>
+            <div class="btn-wrap">
+              <slot name="btnGroup"></slot>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="md-overlay" v-if="mdShow" @click="closeModal"></div>
+    </div>
+</template>
+<script>
+    export default{
+        name:'modal',
+        props:['mdShow'],
+        methods:{
+          closeModal(){
+            //父子组件传递必须用$emit触发一个事件，这里即close事件
+            this.$emit('close');
+          }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Router from './router.js'
+import './assets/css/base.css'
+import './assets/css/index.css'
+
+Vue.use(VueAxios,axios);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router:Router,
   render: h => h(App),
 }).$mount('#app')
